38f32a287de83b626084983027e23f42db336c27
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-01-27 19:31:30 +0100

    added DraggableDiv class

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function randomHexColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
 function dragStartHandler(e) {
   console.log('drag started');
 
@@ -23,3 +27,26 @@ function dropHandler(e) {
   droppable.appendChild(draggableElement);
   // e.dataTransfer.clearData();
 }
+
+class DraggableDiv {
+  constructor(id, width, height) {
+    // create
+    this.element = document.createElement('div');
+    this.element.id = 'drag' + id;
+
+    // style
+    this.style = this.element.style;
+    this.style.width = width + 'px';
+    this.style.height = height + 'px';
+    this.color = randomHexColor();
+    this.style.backgroundColor = this.color;
+
+    // allow drag
+    this.element.draggable = true;
+    this.element.setAttribute('ondragstart', 'dragStartHandler(event)');
+
+    document.getElementById('dragContainer').appendChild(this.element);
+  }
+}
+
+let div = new DraggableDiv(1, 100, 100);
