afafc3e22b212001fe1783faf05dfa674b00501b
sergio núñez meneses <63654322+sergio-nunez-meneses@users.noreply.github.com>
2021-01-27 17:15:40 +0100

    added dragOverHandler function

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function dragStartHandler(e) {
   e.dataTransfer.setData('text/plain', e.target.id);
   e.currentTarget.style.backgroundColor = '#f0ad4e';
 }
+
+function dragOverHandler(e) {
+  console.log('dragging element');
+
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+}
